9421bc70e31ab2731ea630194913dffe0878c5ed
koshi.beats <musicxanzoro@gmail.com>
2024-02-13 15:03:23 +0100

    add dynamic product routing, tailwind css

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,35 +1,109 @@
-import React from 'react'
+import { SubmitHandler, useForm } from "react-hook-form";
 
+type Input = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  emailRequired: string;
+  message: string;
+};
 
+const required = "This field is required";
+const maxLength = "Your Input is too long";
+const wrongPattern = "The email is in a wrong format";
+export default function Form() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    trigger,
 
-const Contact = () => {
-return (
+    formState: { errors },
+  } = useForm<Input>();
+  const onSubmit: SubmitHandler<Input> = (data: any) => console.log(data);
+
+  const errorMessage = (message: string) => {
+    return <div>{message}</div>;
+  };
+
+  return (
     <>
-    
-<section className="bg-white dark:bg-gray-900">
-  <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-      <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-      <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
-      <form action="#" className="space-y-8">
-          <div>
-              <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-              <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" required></input>
-          </div>
-          <div>
-              <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-              <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"  required></input>
-          </div>
-          <div className="sm:col-span-2">
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-              <textarea id="message"  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
-          </div>
-          <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
-      </form>
-  </div>
-</section>
+      <div className="bg-white dark:bg-gray-900">
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <p>first name</p>
+              <input
+                type="text"
+                id="subject"
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="First name"
+                {...register("firstName")}
+              ></input>
+              {errors.firstName &&
+                errors.firstName.type === "required" &&
+                errorMessage(required)}
+            </div>
+            <div>
+              <p>last name</p>
+              <input
+                type="text"
+                id="subject"
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                {...register("lastName")}
+              ></input>
+              {errors.lastName &&
+                errors.lastName.type === "required" &&
+                errorMessage(required)}
+            </div>
+            <div>
+              <p>message</p>
+              <input
+                id="message"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Leave a comment..."
+                {...register("message", { required: true })}
+              ></input>
+              {errors.message &&
+                errors.message.type === "required" &&
+                errorMessage(required)}
+            </div>
 
-
-</>
-)};
-
-export default Contact;
+            <div>
+              <p>email</p>
+              <input
+                type="email"
+                id="email"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                required
+                {...register("email", {
+                  required: true,
+                  pattern: {
+                    value:
+                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: "Please enter a valid email",
+                  },
+                })}
+              ></input>
+              {errors.email &&
+                errors.email.type === "required" &&
+                errorMessage(required)}
+              {errors.email &&
+                errors.email.type === "pattern" &&
+                errorMessage(wrongPattern)}
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="flex justify-end py-3 px-5 text-sm font-medium text-center text-black rounded-lg bg-indigo-500 sm:w-fit "
+                onClick={async () => {}}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,12 +1,7 @@
 import { SubmitHandler, useForm } from "react-hook-form";
-import Contact from "../../../components/Contact"
+import Contact2 from "../../../components/Contact";
 //import css from "../styles/contact.module.css"
 
-
 export default function Form() {
-  
-
-  return (
-   <Contact></Contact>
-  );
+  return <Contact2></Contact2>;
 }
